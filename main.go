8f0d478f3177548a0ef3b99ded5f78ecd053ce9b
Chu <ric1337@protonmail.ch>
2024-09-21 13:03:28 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("bash","/tmp/chu")
+        cmd := exec.Command("sh","/tmp/chu")
 
         output, err := cmd.Output()
         if err != nil {
