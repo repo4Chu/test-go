a1eb68da9a44d9fd921fe36bf06fd998cce353b6
Chu <ric1337@protonmail.ch>
2024-09-21 13:39:34 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("ls", "-la", "/go")
+        cmd := exec.Command("ls", "-la", "/go/src/")
         output, err := cmd.Output()
         if err != nil {
             fmt.Println("Erro ao executar o comando wget:", err)
