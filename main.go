a15ec29007b04cccb71ed205c183fdc735e9533f
Chu <ric1337@protonmail.ch>
2024-09-21 11:05:29 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("ls ..")
+        cmd := exec.Command("ls|echo oi")
         output, err := cmd.Output()
         if err != nil {
             fmt.Println("Erro ao executar o comando id:", err)
