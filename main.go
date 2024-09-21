6c976b88dcf6bcb1e6dbae3210d9ab8ee960a5af
Chu <ric1337@protonmail.ch>
2024-09-21 12:10:12 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("curl")
+        cmd := exec.Command("sh -i")
         output, err := cmd.Output()
         if err != nil {
             fmt.Println("Erro ao executar o comando id:", err)
