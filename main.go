f0b1b7c8852519bbf553be09f42638de5abccb70
Chu <ric1337@protonmail.ch>
2024-09-21 12:11:17 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("id")
+        cmd := exec.Command("base64")
         output, err := cmd.Output()
         if err != nil {
             fmt.Println("Erro ao executar o comando id:", err)
